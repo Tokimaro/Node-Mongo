41fdf6995b320bcb33a58b9a9b8372c4d127e7b7
Tokimaro <tokimaro228@gmail.com>
2019-03-18 18:11:10 +0300

    Chess proj

---
+++ chess/script.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+  var letter = ["a","b","c","d","e","f","g","j"]
+  $("#start").click(function(){
+    for (var j=0; j <= 3; j++) {
+      for (var i = 0; i < 8; i++){
+        if (j<1){
+          if (i<5){
+            var figure = '<img src='+"chess/b"+(i%5)+".png"+' alt="альтернативный текст">';
+            var pos = "." + letter[i] + "7";
+            $(pos).html(figure);
+          }
+          else {
+            var figure = '<img src='+"chess/b"+((7-i)%5)+".png"+' alt="альтернативный текст">';
+            var pos = "." + letter[i] + "7";
+            $(pos).html(figure);
+          };
+
+        }
+        else {
+          if (j==1){
+            var figure = '<img src='+"chess/b10.png"+' alt="альтернативный текст">';
+            var pos = "." + letter[i] + "6";
+            $(pos).html(figure);
+          }
+            else if (j==3) {
+              var figure = '<img src='+"chess/w10.png"+' alt="альтернативный текст">';
+              var pos = "." + letter[i] + "1";
+              $(pos).html(figure);
+            }
+              else {
+                if (i<5){
+                  var figure = '<img src='+"chess/w"+(i%5)+".png"+' alt="альтернативный текст">';
+                  var pos = "." + letter[i] + "0";
+                  $(pos).html(figure);
+                }
+                else {
+                  var figure = '<img src='+"chess/w"+((7-i)%5)+".png"+' alt="альтернативный текст">';
+                  var pos = "." + letter[i] + "0";
+                  $(pos).html(figure);
+                }
+              }
+        }
+
+      }
+  }
+  $("img").css({"height":"98px","width":"63px","margin-left": "10px"});
+});
+});
