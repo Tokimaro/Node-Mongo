159329bc0abd8c554416991f66fbe6b74289e04f
root <root@localhost.localdomain>
2019-03-28 15:03:19 +0300

    Edit

--- array.js
+++
@@ -1,8 +0,0 @@
-var array_counter = function (array) {
-  return "В масиве находится " + array.length + " elems";
-};
-
-var multiply = function (x,y) {
-};
-module.exports.array_counter = array_counter;
-module.exports.multiply = multiply;
